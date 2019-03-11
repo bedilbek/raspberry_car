70ae171ee7ad7e6c2199702df60e2789744e4c1c
bedilbek <bedilbek@gmail.com>
2019-03-11 17:31:59 +0500

    Add camera capability

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include <cstdio>
+#include <iostream>
 #include <wiringPi.h>
+#include "raspicam/raspicam_cv.h"
+#include <fstream>
+#include <ctime>
+#include "opencv2/opencv.hpp"
 
 #define IN1_PIN 1
 #define IN2_PIN 4
@@ -15,55 +20,33 @@ void stop_dc_motor();
 
 int main() {
 
-    if ( wiringPiSetup() == -1 )
+    cv::Mat image, output;
+
+    raspicam::RaspiCam_Cv camera;
+    camera.set( CV_CAP_PROP_FORMAT, CV_8UC1 );
+    camera.set ( cv::CAP_PROP_FRAME_WIDTH,  160);
+    camera.set ( cv::CAP_PROP_FRAME_HEIGHT, 120);
+    camera.set ( cv::CAP_PROP_BRIGHTNESS, 35);
+    camera.set ( cv::CAP_PROP_CONTRAST , 35);
+    camera.set ( cv::CAP_PROP_SATURATION, 35);
+    camera.set ( cv::CAP_PROP_GAIN,  35);
+    camera.set ( cv::CAP_PROP_FPS, 12);
+    camera.open();
+
+    if( !camera.isOpened() )
+    {
+        std::cout << "Could not initialize capturing...\n";
         return 0;
-    pinMode(IN1_PIN, OUTPUT);
-    pinMode(IN2_PIN, OUTPUT);
-    pinMode(IN3_PIN, OUTPUT);
-    pinMode(IN4_PIN, OUTPUT);
-
-    init_dc_motor();
-
-    int counter = 0;
-    go_forward();
-    while (counter != 4)
-    {
-        delay(1000);
-        printf("forward\n");
-        counter++;
     }
 
-    counter = 0;
-    go_left();
-    while (counter != 4)
-    {
-        delay(1000);
-        printf("forward\n");
-        counter++;
-    }
+    while(1){
+        camera.grab();
+        camera.retrieve(output);
 
-    counter = 0;
-    go_right();
-    while (counter != 4)
-    {
-        delay(1000);
-        printf("forward\n");
-        counter++;
-    }
-
-    counter = 0;
-    go_backward();
-    while (counter != 4)
-    {
-        delay(1000);
-        printf("forward\n");
-        counter++;
-    }
-
-    stop_dc_motor();
-
-    return 0;
-}
+        imshow("webcam input", output);
+        char c = (char)cv::waitKey(10);
+        if( c == 27 ) break;
+    }}
 
 
 void init_dc_motor()
