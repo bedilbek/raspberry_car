8824f14c312158614f4d1b7c91c255941d0b8579
Sherzod <asdfgh5889@gmail.com>
2019-04-16 02:32:53 +0500

    Global variables

---
+++ lane_detector/globals.cpp
@@ -0,0 +1,5 @@
+#include "globals.h"
+
+float ym_per_pix = 30 / 720;
+float xm_per_pix = 3.7 / 700;
+int time_window = 10;
---
+++ lane_detector/globals.h
@@ -0,0 +1,6 @@
+#ifndef GLOBAL_VALS
+#define GLOBAL_VALS
+extern float ym_per_pix;
+extern float xm_per_pix;
+extern int time_window;
+#endif
