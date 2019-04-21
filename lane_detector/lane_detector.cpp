7bb714ebd125e5d56e9f2b66fa5fabac5a54fa88
bedilbek <bedilbek@gmail.com>
2019-04-21 20:16:49 +0500

    Lane detector debugging

--- lane_detector/lane_detector.cpp
+++ lane_detector/lane_detector.cpp
@@ -6,6 +6,7 @@
 #include "bird_eye.h"
 #include "line_utils.h"
 #include <opencv2/opencv.hpp>
+#include "globals.h"
 #include <iostream>
 #include <vector>
 
@@ -14,15 +15,19 @@ using namespace cv;
 
 Mat src_gray, detected_edges;
 Mat frame;
+Mat bird_forw, bird_back, img_bird, img_bin, blend_on_road, blend_output;
 
 int lowThreshold;
 int const max_lowThreshold = 100;
 int ratio = 3;
 int kernel_size = 3;
 char* window_name = "Edge Map";
-
+double offset = 0;
 int playback_slider = 0;
 VideoCapture video;
+
+array<Line, 2> lines = {Line(time_window), Line(time_window)};
+int processed_frames = 0;
 
 void on_canny_threshold_change(int, void *)
 {
@@ -32,6 +37,26 @@ void on_canny_threshold_change(int, void *)
 void on_playback_slidebar_change(int, void *)
 {
 	video.set(CAP_PROP_POS_FRAMES, playback_slider);
+}
+
+void process_pipeline(Mat frame, bool keep_state = true)
+{
+    img_bin = binarize(frame, false);
+
+    img_bird = bird_eye(img_bin, bird_forw, bird_back, false);
+
+    if (processed_frames > 0 && keep_state && lines[0].detected && lines[1].detected)
+    	get_fits_by_previous_fits(img_bird, lines[0], lines[1], false);
+    else
+		lines = get_fits_by_sliding_windows(img_bird);
+
+	offset = compute_offset_from_center(lines[0], lines[1], frame.cols);
+
+	blend_on_road = draw_back_onto_the_road(frame, img_bin, lines[0], lines[1], keep_state);
+
+	imshow("blend", blend_on_road);
+	processed_frames += 1;
+
 }
 
 int main()
@@ -61,15 +86,16 @@ int main()
 
 //			Mat binarized = binarize(frame, true);
 		}
-		cvtColor(frame, src_gray, COLOR_BGR2GRAY);
+//		cvtColor(frame, src_gray, COLOR_BGR2GRAY);
 
 		/// Reduce noise with a kernel 3x3
-		blur( src_gray, detected_edges, Size(3,3) );
+//		blur( src_gray, detected_edges, Size(3,3) );
 
 		/// Canny detector
-		Canny( detected_edges, detected_edges, lowThreshold, lowThreshold*ratio, kernel_size );
+//		Canny( detected_edges, detected_edges, lowThreshold, lowThreshold*ratio, kernel_size );
 
-		imshow(window_name, detected_edges);
+//		imshow(window_name, detected_edges);
+		process_pipeline(frame, true);
 //			Mat forward, backward;
 //			Mat birdeye = bird_eye(binarized, forward, backward, false);
 //			Mat birdeye_org = bird_eye(frame, forward, backward, false);
@@ -79,7 +105,7 @@ int main()
 
 //			resize(birdeye, birdeye, Size(birdeye.cols / 2, birdeye.rows / 2));
 		//resize(birdeye_org, birdeye_org, Size(birdeye.cols / 2, birdeye.rows / 2));
-		imshow(window_name, detected_edges);
+//		imshow(window_name, detected_edges);
 		imshow("Org", frame);
 
 		// Press  ESC on keyboard to exit
--- lane_detector/line_utils.h
+++ lane_detector/line_utils.h
@@ -39,6 +39,7 @@ public:
 
 	double curvature_meter();
 
+	Mat draw(Mat mask, Scalar color = {255, 0, 0}, int line_width = 50, bool average = false);
 };
 
 /**
@@ -53,7 +54,7 @@ public:
 */
 array<Line, 2> get_fits_by_sliding_windows(Mat birdeye_binary, int n_windows = 9, bool verbose = false);
 
-/*
+/**
 	Compute offset from center of the inferred lane.
 	The offset from the lane center can be computed under the hypothesis that the camera is fixed
 	and mounted in the midpoint of the car roof. In this case, we can approximate the car's deviation
@@ -67,6 +68,60 @@ array<Line, 2> get_fits_by_sliding_windows(Mat birdeye_binary, int n_windows = 9
 */
 double compute_offset_from_center(Line line_lt, Line line_rt, int frame_width);
 
+/**
+Get polynomial coefficients for lane-lines detected in an binary image.
+This function starts from previously detected lane-lines to speed-up the search of lane-lines in the current frame.
+
+:param birdeye_binary: input bird's eye view binary image
+:param line_lt: left lane-line previously detected
+:param line_rt: left lane-line previously detected
+:param verbose: if True, display intermediate output
+:return: updated lane lines and output image
+*/
 void get_fits_by_previous_fits(Mat birdeye_binary, Line line_lt, Line line_rt, bool verbose = false);
+
+/**
+Draw both the drivable lane area and the detected lane-lines onto the original (undistorted) frame.
+:param img_undistorted: original undistorted color frame
+:param Minv: (inverse) perspective transform matrix used to re-project on original frame
+:param line_lt: left lane-line previously detected
+:param line_rt: right lane-line previously detected
+:param keep_state: if True, line state is maintained
+:return: color blend
+*/
+Mat draw_back_onto_the_road(Mat img_undistorted, Mat Minv, Line line_lt, Line line_rt, bool keep_state);
+
+/**
+Return evenly spaced numbers over a specified interval.
+
+Returns `num` evenly spaced samples, calculated over the
+        interval [`start`, `stop`].
+
+The endpoint of the interval can optionally be excluded.
+
+Parameters
+----------
+start : double
+        The starting value of the sequence.
+stop : double
+        The end value of the sequence, unless `endpoint` is set to False.
+In that case, the sequence consists of all but the last of ``num + 1``
+evenly spaced samples, so that `stop` is excluded.  Note that the step
+size changes when `endpoint` is False.
+num : int, optional
+        Number of samples to generate. Default is 50. Must be non-negative.
+endpoint : bool, optional
+        If True, `stop` is the last sample. Otherwise, it is not included.
+Default is True.
+
+Returns
+-------
+samples : vector<double>
+        There are `num` equally spaced samples in the closed interval
+``[start, stop]`` or the half-open interval ``[start, stop)``
+(depending on whether `endpoint` is True or False).
+*/
+vector<double> linspace(double start, double stop, int num, bool endpoint = true);
+
 
 #endif
